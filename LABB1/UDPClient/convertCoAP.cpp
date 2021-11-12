e8e278d67166e5cee4abdf95a27ae63f073e68e5
Manjaro97 <j_c_kc@live.se>
2021-11-12 16:59:04 +0100

    Started working on Msg To CoAP

--- LABB1/UDPClient/convertCoAP.cpp
+++ LABB1/UDPClient/convertCoAP.cpp
@@ -1,10 +1,159 @@
+#include <iostream>
 #include "convertCoAP.h"
 
-std::vector<char> toCoAP(std::vector<std::string>){
+std::vector<char> toCoAP(std::vector<std::string> inputMsg){
     
     std::vector<char> msgCoAP;
 
+    // Version conversion
+    if(inputMsg[0] == "1"){
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('1');
+    }
+    else{
+        std::cout << "ERROR: Invalid Version" << std::endl;
+        return msgCoAP;
+    }
 
+    // Message type conversion
+    if(inputMsg[1] == "CON"){
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+    }
+    else if(inputMsg[1] == "NON"){
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('1');
+    }
+    else if(inputMsg[1] == "ACK"){
+        msgCoAP.push_back('1');
+        msgCoAP.push_back('0');
+    }
+    else if(inputMsg[1] == "RST"){
+        msgCoAP.push_back('1');
+        msgCoAP.push_back('1');
+    }
+    else{
+        std::cout << "ERROR: Invalid Message Type T" << std::endl;
+        return msgCoAP;
+    }
+
+    // TKL conversion
+    if(inputMsg[3].length() == 0){
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+    }
+    //TODO: FIX THIS
+    int TKL = inputMsg[3].length();
+        
+    // Request Conversion
+    if(inputMsg[2] == "EMPTY"){
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+    }
+    else if(inputMsg[2] == "GET"){
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('1');
+    }
+    else if(inputMsg[2] == "POST"){
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('1');
+        msgCoAP.push_back('0');
+    }
+    else if(inputMsg[2] == "PUT"){
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('1');
+        msgCoAP.push_back('1');
+    }
+    else if(inputMsg[2] == "DELETE"){
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('1');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+    }
+    else{
+        std::cout << "ERROR: Invalid Request" << std::endl;
+        return msgCoAP;
+    }
+
+    //Create Random MEssage ID
+    msgCoAP.push_back('0');
+    msgCoAP.push_back('1');
+    msgCoAP.push_back('0');
+    msgCoAP.push_back('0');
+    msgCoAP.push_back('0');
+    msgCoAP.push_back('1');
+    msgCoAP.push_back('0');
+    msgCoAP.push_back('0');
+    msgCoAP.push_back('0');
+    msgCoAP.push_back('1');
+    msgCoAP.push_back('0');
+    msgCoAP.push_back('0');
+    msgCoAP.push_back('1');
+    msgCoAP.push_back('1');
+    msgCoAP.push_back('0');
+    msgCoAP.push_back('0');
+
+    // Token ceonversion
+    std::string token =  inputMsg[3];
+
+    // OptionDelta conversion
+    if(inputMsg[4] == "Uri-Path"){
+        msgCoAP.push_back('1');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('1');
+        msgCoAP.push_back('1');
+    }
+    else{
+        std::cout << "ERROR: Invalid Option Delta" << std::endl;
+        return msgCoAP;
+    }
+
+    // Option length
+    if(inputMsg[5].length() >= 0){
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('1');
+        msgCoAP.push_back('0');
+        msgCoAP.push_back('0');
+    }
+
+    // Option Delta (extended)
+
+    // Option length (extended) 
+
+    // Option Value
+    std::string path = inputMsg[5];
+    
+
+
+    return msgCoAP;
 }
 
 std::vector<std::string> fromCoAP(std::vector<char>){
--- LABB1/UDPClient/main.cpp
+++ LABB1/UDPClient/main.cpp
@@ -16,22 +16,27 @@ int main(){
         //Test Message
         std::vector<std::string> tester;
         std::string testMsg = "Hello World";
-        std::string ver;
+        std::string ver = "1";
+        //Ver is [0]
         tester.push_back(ver); // CoAP version: (01) is version 1
         std::string T = "NON";
+        //T is [1]
         tester.push_back(T); // Message type: CON (00), NON (01), ACK (10) and RST (11)
-        std::string TKL;
-        tester.push_back(TKL); // TKL is token length: Not needed, (you can it set to 0000)
         std::string code = "GET";
+        //code is [2]
         tester.push_back(code); // Method: Empty (0000 0000), GET (0000 0001), POST (0000 0010), PUT (0000 0011), DELETE (0000 0100)
-        std::string msgID;
-        tester.push_back(msgID); // Message ID
-        std::string Token;
+        std::string Token = "";
+        //Token is [3]
         tester.push_back(Token); // Token
-        std::string Options;
-        tester.push_back(Token); // Options
-        std::string Payload;
-        tester.push_back(Token); // Payload
+        std::string OptionDelta = "Uri-Path";
+        //OptionDelta is [4]
+        tester.push_back(OptionDelta); // Options
+        std::string OptionValue = "/path/";
+        //OptionValue is [5]
+        tester.push_back(OptionValue); // Options
+        std::string Payload = "";
+        //Payload is [6]
+        tester.push_back(Payload); // Payload
         //----------------------------------
 
 
