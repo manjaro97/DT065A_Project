a354927bf13bcf7764d54d01f29ab23778ccfefb
Manjaro97 <j_c_kc@live.se>
2021-12-08 01:16:09 +0100

    Debugging

--- Labb2/MQTTServer/SubscribeHandler.cpp
+++ Labb2/MQTTServer/SubscribeHandler.cpp
@@ -10,10 +10,12 @@ std::vector<char> DecodeSubscribe(std::string msg){
     msg.erase(0, 8);
     //VariableLength
     int lengthVariableHeader = std::bitset<8>(msg.substr(0, 8)).to_ulong();
+    std::cout << lengthVariableHeader << std::endl;
     msg.erase(0, 8);
 
     //Package Identifier
     int packageID = std::bitset<16>(msg.substr(0, 16)).to_ulong();
+    std::cout  << "packageID: " << packageID << std::endl;
     msg.erase(0, 16);
     lengthVariableHeader -= 16;
 
@@ -26,11 +28,13 @@ std::vector<char> DecodeSubscribe(std::string msg){
     while(i < lengthVariableHeader)
     {
         bodyLength = std::bitset<16>(msg.substr(0, 16)).to_ulong();
+        std::cout << "bodyLength: " << bodyLength << std::endl;
         msg.erase(0, 16);
         i+=16;
         body = "";
         while(std::bitset<8>(msg.substr(0, 6)).to_string() != "000000"){
             c = char(std::bitset<8>(msg.substr(0, 8)).to_ulong());
+            std::cout << "c: " << c << std::endl;
             body += c;
             msg.erase(0, 8);
             i+=8;
@@ -38,6 +42,7 @@ std::vector<char> DecodeSubscribe(std::string msg){
         //Body Ender
         msg.erase(0, 6);
         QoS = std::bitset<8>(msg.substr(0, 2)).to_ulong();
+        std::cout  << "QoS: " << QoS << std::endl;
         i+=8;
     }
     
@@ -57,9 +62,10 @@ std::vector<char> SendSubAck(){
 }
 
 std::vector<char> DecodeUnsubscribe(std::string msg){
-    
+    return SendUnsubAck();
 }
 
 std::vector<char> SendUnsubAck(){
-    
+    std::vector<char> bytesVector;
+    return bytesVector;
 }
