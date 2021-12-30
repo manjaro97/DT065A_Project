1b13a252825078a4b61ee3fea3d760992a1e1daf
Manjaro97 <j_c_kc@live.se>
2021-12-30 17:06:45 +0100

    Project Fully done

--- Project/Sensor_Program/main.py
+++ Project/Sensor_Program/main.py
@@ -1,56 +1,46 @@
 import datetime
 import logging
+
 import asyncio
+
 import aiocoap.resource as resource
 import aiocoap
 
-from sensor_data import sensor
+import random
 
 HOST = '127.0.0.1'
 PORT = 5683
 
-class temperature_resource(resource.Resource):
-    def __init__(self):
-        super().__init__()
-        self.sensor = sensor(mean=20, sigma=2, value=20)
-
+class GetTemp(resource.Resource):
+    
     async def render_get(self, request):
-        payload = self.sensor.get_sensor_data()
-        print(f'CoAP client| GET temperature with payload: {payload.decode()}')
+
+        payload = str(int(random.gauss(20, 2))).encode('utf-8')
         return aiocoap.Message(payload=payload)
 
-class humidity_resource(resource.Resource):
-    def __init__(self):
-        super().__init__()
-        self.sensor = sensor(mean=40, sigma=5, value=20)
-
+class GetHum(resource.Resource):
+    
     async def render_get(self, request):
-        payload = self.sensor.get_sensor_data()
-        print(f'CoAP client| GET humidity with payload: {payload.decode()}')
+
+        payload = str(int(random.gauss(40, 5))).encode('utf-8')
         return aiocoap.Message(payload=payload)
+
+# logging setup
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("coap-server").setLevel(logging.DEBUG)
 
 async def main():
-
     # Resource tree creation
     root = resource.Site()
-    root.add_resource(['temperature'], temperature_resource())
-    root.add_resource(['humidity'], humidity_resource())
 
-    # Binds context to all adresses on CoAP port
+    root.add_resource(['temperature'], GetTemp())
+    root.add_resource(['humidity'], GetHum())
+
     await aiocoap.Context.create_server_context(bind=(HOST,PORT),site=root)
 
-    # Server loop
+    # Run forever
     await asyncio.get_running_loop().create_future()
 
-
-def run_server():
-    print(f"CoAP server| Starting on port {PORT}")
-
-    # logging setup
-    logging.basicConfig(level=logging.INFO)
-    logging.getLogger("coap-server").setLevel(logging.INFO)
-
-    asyncio.run(main())
-
 if __name__ == "__main__":
-    run_server()
+    asyncio.run(main())
